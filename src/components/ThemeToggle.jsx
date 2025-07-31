9e6d77cd4361afdcc5f48e65f994aa6b6c8e173c
Dannysommar <danny.sommar@gmail.com>
2025-07-31 23:50:03 +0100

    added some tailwindcss and ThemeToggle buttin is working

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,18 @@
+import { Sun, Moon } from "lucide-react"
 import {useState} from 'react'
 
 export default function ThemeToggle() {
 
-    const [isDarkMode, setDarkMode] = useState(false)
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return <button></button>
+    function toggleTheme() {
+        if (isDarkMode)
+            document.documentElement.classList.remove("dark");
+        else
+            document.documentElement.classList.add("dark");
+        
+        setIsDarkMode(prev => !prev);
+    }
+
+    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
 }
